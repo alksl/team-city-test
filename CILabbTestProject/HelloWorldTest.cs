ae48f084a081224e8bd1f318c0975ea35d1e2338
Anton Holmgren <me@antonholmgren.com>
2014-09-15 09:46:27 +0200

    Happy hacking

---
+++ CILabbTestProject/HelloWorldTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CILabbTestProject
+{
+    [TestFixture]
+    class HelloWorldTest
+    {
+        [Test]
+        public void say_hello_test()
+        {
+            var hello = new HelloWorld();
+            Assert.AreEqual("Hello World!", hello.SayHello());
+        }
+    }
+}
---
+++ CILabbTestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CILabbTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = new HelloWorld();
+            Console.WriteLine(hello.SayHello());
+        }
+    }
+}
